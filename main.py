4bb2d24a0698a3e36523bb64a492e981651b49de
Amanjit Singh <a.singh.uh@gmail.com>
2022-07-16 03:08:08 -0500

    file uploaded

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from sklearn import tree
 import pandas as pd
-import os 
 
-os.chdir(r'C:\Users\Aman\Desktop\PROJECTS\Covidata')
-dataSet = pd.read_excel('WHO_COVID_Excess_Deaths_EstimatesByIncome.xlsx')
+#Read in the data, specifically sheet 
+dataSet = pd.read_excel(r'Covidata\WHO_COVID_Excess_Deaths_EstimatesByIncome.xlsx', "Deaths by year and month", usecols="A:C,F:G", skiprows=11)
+print(dataSet)
+#Separate 
