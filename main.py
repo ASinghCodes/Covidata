9ab64157d0003d089c627a19312dc38f437c1b1e
Amanjit Singh <a.singh.uh@gmail.com>
2022-07-16 02:27:13 -0500

    upload

---
+++ main.py
@@ -0,0 +1,6 @@
+from sklearn import tree
+import pandas as pd
+import os 
+
+os.chdir(r'C:\Users\Aman\Desktop\PROJECTS\Covidata')
+dataSet = pd.read_excel('WHO_COVID_Excess_Deaths_EstimatesByIncome.xlsx')
