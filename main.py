50c6e173e65b33ec45c8e5220e7637c1ba24ebc5
Amanjit Singh <a.singh.uh@gmail.com>
2022-07-16 19:47:27 -0500

    Data Analysis Update

--- main.py
+++ main.py
@@ -1,7 +1,40 @@
-from sklearn import tree
+from audioop import mul
+from matplotlib import style
+import matplotlib.pyplot as plt
+import seaborn as sns
 import pandas as pd
+from sklearn.preprocessing import scale
 
 #Read in the data, specifically sheet 
-dataSet = pd.read_excel(r'Covidata\WHO_COVID_Excess_Deaths_EstimatesByIncome.xlsx', "Deaths by year and month", usecols="A:C,F:G", skiprows=11)
-print(dataSet)
-#Separate 
+dataSet = pd.read_excel(r'Covidata\WHO_COVID_Excess_Deaths_EstimatesByIncome.xlsx', "Deaths by year and month", usecols="A:J", skiprows=11)
+#print(dataSet)
+#What exists or does not exist within the dataset
+
+info = dataSet.info()
+nullInfo = dataSet.isnull()
+
+#Lets make some terms more readable
+
+dataSet.income.replace(['LIC', 'LMIC', 'UMIC', 'HIC'], ['Low-Income', 'Lower-Middle', 'Upper-Middle', 'High'], inplace=True)
+
+#Now that we have cleaned up the data to be a bit more understandable, lets go for visualization
+sns.set(style="darkgrid")
+fig, axs = plt.subplots(2, 2, figsize=(10,10))
+expMeanPlot = sns.histplot(data=dataSet, x= 'expected_mean', hue='year', kde=True, ax=axs[0,0], shrink=0.5)
+acmMeanPlot = sns.histplot(data=dataSet, x= 'acm_mean', hue='year', kde=True, ax=axs[0,1], shrink=0.5)
+excMeanPlot = sns.histplot(data=dataSet, x= 'excess_mean', hue='year', kde=True, ax=axs[1,0], shrink=0.5)
+cExcMeanPlot = sns.histplot(data=dataSet, x= 'cumul_excess_mean', hue='year', kde=True, ax=axs[1,1], shrink=0.5)
+plt.show()
+
+#Lets look at the cumulative impact of excess deaths by country
+sns.set(style="darkgrid")
+plt.figure(figsize=(10,10))
+plt.title('Excess mean deaths by country income due to COVID-19 (Cumulative)')
+excMeanIncPlot = sns.histplot(data=dataSet, x= 'excess_mean', hue='income', multiple='dodge', kde=True, shrink=0.5)
+plt.show()
+
+sns.set(style="darkgrid")
+plt.figure(figsize=(10,10))
+plt.title('Excess mean deaths by month due to COVID-19 (Cumulative)')
+excMeanMonPlot = sns.barplot(y=dataSet['excess_mean'], x=dataSet['month'])
+plt.show()
--- readme.md
+++ readme.md
@@ -2,4 +2,9 @@ The data used here is from the World Health Organization (WHO)
 Data can be found here directly:
 https://www.who.int/data/sets/global-excess-deaths-associated-with-covid-19-modelled-estimates
 
-Dependency needed: scikit-learn, pandas
+Disclaimer: some column names were changed due to ease of use, for example: excess.mean -> excess_mean and so forth
+Dependency needed: matplotlib, seaborn, pandas, openpyxl
+
+![Alt Text](Covidata\Figure_1.png)
+![Alt Text](Covidata\Figure_2.png)
+![Alt Text](Covidata\Figure_3.png)
